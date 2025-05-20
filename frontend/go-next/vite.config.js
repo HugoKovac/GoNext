6eed0c3d3ad3ad38707a7168928927302f46c907
h.kovac <h.kovac@eyeo.com>
2025-05-20 11:38:14 +0200

    add tailwind + daisy

--- frontend/go-next/vite.config.js
+++ frontend/go-next/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+
 
 const backendUrl = process.env.BACKEND_URL || "http://backend_dev:8080";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {
