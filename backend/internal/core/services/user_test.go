1d4817d7db9ba32cb3dd0c3c51e5ed3ccf3f0f61
h.kovac <h.kovac@eyeo.com>
2025-06-01 16:04:45 +0200

    test find by email and by id

--- backend/internal/core/services/user_test.go
+++ backend/internal/core/services/user_test.go
@@ -68,3 +68,69 @@ func TestRegisterEmailAlreadyExists(t *testing.T){
 	assert.Nil(t, rtnUser)
 	assert.NotNil(t, err)
 }
+
+func TestGetById(t *testing.T){
+	mockUserRepo := mocks.NewMockUserRepository(t)
+	user := domain.User{
+		Id: "55",
+		Email: "test55@email.com",
+		Password: "pass",
+		CreatedAt: time.Date(2025,05,4,5,13,24,0,time.Now().Local().Location()),
+		UpdatedAt: time.Date(2025,05,4,5,13,24,0,time.Now().Local().Location()),
+		Role: "user",
+
+	}
+	mockUserRepo.On("FindById", user.Id).Return(&user, nil)
+	service := NewUserService(mockUserRepo)
+	rtnUser, err := service.GetById(user.Id)
+	assert.Nil(t, err)
+	assert.Equal(t, user.CreatedAt, rtnUser.CreatedAt)
+	assert.Equal(t, user.UpdatedAt, rtnUser.UpdatedAt)
+	assert.Equal(t, user.Id, rtnUser.Id)
+	assert.Equal(t, user.Email, rtnUser.Email)
+	assert.Equal(t, user.Password, rtnUser.Password)
+	assert.Equal(t, user.Role, rtnUser.Role)
+
+}
+
+func TestGetByIdUserDoesntExists(t *testing.T){
+	mockUserRepo := mocks.NewMockUserRepository(t)
+	mockUserRepo.On("FindById", "44").Return(nil, errors.New("Doesn't exists"))
+	service := NewUserService(mockUserRepo)
+	rtnUser, err := service.GetById("44")
+	assert.Nil(t, rtnUser)
+	assert.NotNil(t, err)
+}
+
+func TestGetByEmail(t *testing.T){
+	mockUserRepo := mocks.NewMockUserRepository(t)
+	user := domain.User{
+		Id: "55",
+		Email: "test66@email.com",
+		Password: "pass",
+		CreatedAt: time.Date(2025,05,4,5,13,24,0,time.Now().Local().Location()),
+		UpdatedAt: time.Date(2025,05,4,5,13,24,0,time.Now().Local().Location()),
+		Role: "user",
+
+	}
+	mockUserRepo.On("FindByEmail", user.Email).Return(&user, nil)
+	service := NewUserService(mockUserRepo)
+	rtnUser, err := service.GetByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.CreatedAt, rtnUser.CreatedAt)
+	assert.Equal(t, user.UpdatedAt, rtnUser.UpdatedAt)
+	assert.Equal(t, user.Id, rtnUser.Id)
+	assert.Equal(t, user.Email, rtnUser.Email)
+	assert.Equal(t, user.Password, rtnUser.Password)
+	assert.Equal(t, user.Role, rtnUser.Role)
+
+}
+
+func TestGetByEmailUserDoesntExists(t *testing.T){
+	mockUserRepo := mocks.NewMockUserRepository(t)
+	mockUserRepo.On("FindByEmail", "nexists@gmail.com").Return(nil, errors.New("Doesn't exists"))
+	service := NewUserService(mockUserRepo)
+	rtnUser, err := service.GetByEmail("nexists@gmail.com")
+	assert.Nil(t, rtnUser)
+	assert.NotNil(t, err)
+}
