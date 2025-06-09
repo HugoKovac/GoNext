dd1ea3953c0b87d612ce1364afd03f9008892e19
hugokovac <kovmaxime@gmail.com>
2025-06-09 19:23:31 +0200

    add dependencies for buckets

--- frontend/terraform/bucket.tf
+++ frontend/terraform/bucket.tf
@@ -8,6 +8,7 @@ resource "aws_s3_bucket_public_access_block" "acl" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+  depends_on = [ aws_s3_bucket.b ]
 }
 
 resource "aws_s3_bucket_policy" "p" {
@@ -25,6 +26,7 @@ resource "aws_s3_bucket_policy" "p" {
       }
     ]
   })
+  depends_on = [ aws_s3_bucket_public_access_block.acl ]
 }
 
 resource "aws_s3_object" "static_site_upload_object" {
@@ -51,6 +53,7 @@ resource "aws_s3_object" "static_site_upload_object" {
     ".ttf"  = "font/ttf"
     ".otf"  = "font/otf"
   }, ".${reverse(split(".", each.value))[0]}", "application/octet-stream")
+  depends_on = [ aws_s3_bucket.b ]
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
@@ -63,5 +66,7 @@ resource "aws_s3_bucket_website_configuration" "website" {
   error_document {
     key = "index.html"
   }
+
+  depends_on = [ aws_s3_object.static_site_upload_object ]
 }
 
